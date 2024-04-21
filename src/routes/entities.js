720cbd9496c62c2e0bdb3b2e028906ce85ea2496
Cruz28 <154674121+Cruz28-hub@users.noreply.github.com>
2024-04-21 11:48:10 +0100

    Update entities.js

--- src/routes/entities.js
+++ src/routes/entities.js
@@ -5,4 +5,9 @@ router.get('/entities/:id', function(req, res) {
   entities.show(req, res);
 });
 
+// Save entity
+router.post('/save', function(req, res) {
+  entities.save(req, res);
+});
+
 module.exports = router;

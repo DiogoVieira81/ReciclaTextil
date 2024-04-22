47653b50fdb922cf35743ca7d72b40ae01c349ba
Cruz28 <154674121+Cruz28-hub@users.noreply.github.com>
2024-04-22 10:51:40 +0100

    Update das vistas das entities

--- src/routes/entities.js
+++ src/routes/entities.js
@@ -3,13 +3,9 @@ const asyncHandler = require("express-async-handler");
 var router = express.Router();
 const entitiesController = require('../controllers/entityControllers');
 
-router.get('/entities/:id', function(req, res) {
-  entities.show(req, res);
-});
-
 // Create entity
-router.post('/create', function(req, res) {
-  entitiesController.entity_create_post(req, res);
+router.get('/', function(req, res) {
+  res.render('entities/create');
 });
 
 

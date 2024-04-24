58d0d8a71889c6a6788948377290e2f59776815f
DiogoVieira81 <diogovieiraengineer@gmail.com>
2024-04-24 17:17:02 +0100

    Retificação no app.js dos hiperlinks para admin controllers na app.js e criação de middleware adminAuth

---
+++ src/middleware/adminAuth.js
@@ -0,0 +1,19 @@
+// Middleware function for admin authentication
+exports.adminAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if (err) {
+                return res.status(401).json({ message: "Not authorized" });
+            } else {
+                if (decodedToken.role !== "Basic") {
+                    return res.status(401).json({ message: "Not authorized" });
+                } else {
+                    next(); // Proceed to the next middleware or route handler
+                }
+            }
+        });
+    } else {
+        return res.status(401).json({ message: "Not authorized, token not available" });
+    }
+};

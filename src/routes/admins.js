cd350c3cfdf192cacd82da024f6c982ab9dbab38
Cruz28 <154674121+Cruz28-hub@users.noreply.github.com>
2024-04-24 16:56:56 +0100

    Created admin routes and files for viewss

---
+++ src/routes/admins.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+const adminsController = require("../controllers/adminControllers");
+
+router.get("/create", adminsController.admin_create_get);
+
+router.post("/create", adminsController.admin_create_post);
+
+router.get("/update", adminsController.admin_update_get);
+
+router.post("/update", adminsController.admin_update_post);
+
+router.get("/delete", adminsController.admin_delete_get);
+
+router.post("/delete", adminsController.admin_delete_post);
+
+router.get("/login", adminsController.admin_login_get);
+
+router.post("/login", adminsController.admin_login_post);
+
+router.get("/show/:id", adminsController.admin_detail);
+
+router.get('/dashboard', function(req, res) {
+    res.render('admins/dashboard');
+  });
+
+module.exports = router;

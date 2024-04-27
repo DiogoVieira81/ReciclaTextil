4a75b96e4204eb67269d8491482124081a64465d
DiogoVieira81 <diogovieiraengineer@gmail.com>
2024-04-27 17:51:08 +0100

    Dashboard a aparecer

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const connectEnsureLogin = require('connect-ensure-login');
 
 /* GET home page. */
-router.get('/', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+router.get('/', /*connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
   res.render('dashboard');
 });
 

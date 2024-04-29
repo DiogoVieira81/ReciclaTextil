918304c56fec5a4adcae0d83e48d49771a787a20
8140310 <8140310@estg.ipp.pt>
2024-04-29 02:48:59 +0100

    pequenas correcoes

--- src/routes/donors.js
+++ src/routes/donors.js
@@ -1,14 +1,30 @@
 var express = require("express");
 var router = express.Router();
+const multer=require('multer')
 const donorsController = require("../controllers/donorControllers");
 
 router.get("/", function (req, res) {
     res.render("donors");
   });
+// Configuração do Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/", function (req, res, next) {
+  res.render("entities");
+});
 
 router.get("/create", donorsController.donor_create_get);
 
-router.post("/create", donorsController.donor_create_post);
+router.post("/create",upload.single("image"), donorsController.donor_create_post);
 
 router.get("/update/:id", donorsController.donor_update_get);
 

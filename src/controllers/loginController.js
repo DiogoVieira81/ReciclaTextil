d50c4263465383ef1a6d4304b5e355ce145bd32b
8140310 <8140310@estg.ipp.pt>
2024-05-27 23:12:43 +0100

    Adição login ao swagger

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,6 +3,33 @@ const Admin = require('../models/Admin');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+exports.login_session_json = asyncHandler(async (req, res, next) => {
+  try {
+      const { email, password } = req.body;
+
+      const user = await Admin.findOne({ email: email });
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+          const token = jwt.sign({ id: user._id }, 'secretpassword', {
+              expiresIn: '1h',
+          });
+
+          res.cookie('token', token, {
+              expires: new Date(Date.now() + 1 * 60 * 60 * 1000), // 1 hora
+              httpOnly: true,
+          });
+
+          req.session.user_id = user._id;
+
+          return res.json({ message: 'Login successful', token });
+      } else {
+          return res.status(401).json({ message: 'Invalid email or password' });
+      }
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+  }
+});
 exports.login_session = asyncHandler(async (req, res, next) => {
   try {
     const { email, password } = req.body;
--- src/routes/login.js
+++ src/routes/login.js
@@ -11,8 +11,61 @@ router.get('/', function (req, res) {
   res.render('login/logins',{error:''});
 });
 
-
-
+/**
+ * @swagger
+ * /login/api:
+ *   post:
+ *     summary: Rota para login 
+ *     tags: [Login]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *               password:
+ *                 type: string
+ *                 description: The user's password
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Login successful
+ *                 token:
+ *                   type: string
+ *                   example: <JWT token>
+ *       401:
+ *         description: Unauthorized, incorrect email or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid email or password
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
+router.post('/api',loginController.login_session_json);
 router.post('/',loginController.login_session);
   
 

0dfbb6ebe4035d25ed313a31c0d3bf9e9879ebe4
Cruz28 <154674121+Cruz28-hub@users.noreply.github.com>
2024-05-30 15:48:39 +0100

    Save

--- angular-client/src/app/pages/donation-list/donation-list.component.ts
+++ angular-client/src/app/pages/donation-list/donation-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-donation-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './donation-list.component.html',
   styleUrl: './donation-list.component.css'
 })
-export class DonationListComponent {
-  
+export class DonationListComponent implements OnInit{
+  httpClient = inject(HttpClient);
+  data:any = [];
+
+  ngOnInit(): void {
+    this.getDonations();
+  }
+
+  fetchData(){
+    this.httpClient.get('http://localhost:3000/donations/list/api').subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    })
+  }
+
+  getDonations() {
+    this.httpClient.get<any[]>('http://localhost:3000/donations/list/api').pipe(
+      map((response: any) => response.donations)
+    ).subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    })
+  }
 }
--- angular-client/src/app/pages/rest.service.ts
+++ angular-client/src/app/pages/rest.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RestService {
 
-  constructor() { }
+export class RestService {
+  constructor(private httpClient: HttpClient) {}
+
+  getDonations(): Observable<any[]> {
+    return this.httpClient.get<any[]>('http://localhost:3000/donations/list/api').pipe(
+      map((response: any) => response.donations)
+    );
+  }
 }

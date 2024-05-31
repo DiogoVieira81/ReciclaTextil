89c4da12e41cc4376a5c4d7ac597037c1bc9edf7
Diogo Vieira <diogovieiraengineer@gmail.com>
2024-05-31 18:07:19 +0100

    Criacao do model donor.ts

---
+++ angular-client/src/app/models/donor.ts
@@ -0,0 +1,13 @@
+export interface Donor {
+    name: string,
+    email: string,
+    password: string,
+    phoneNumber: string,
+    address : string,
+    city : string,
+    district : string,
+    kg : number,
+    points : number,
+    totalDonations : number,
+    ImageName : string,
+}

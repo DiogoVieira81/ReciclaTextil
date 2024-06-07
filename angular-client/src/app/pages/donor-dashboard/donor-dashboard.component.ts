43360c9128e9aafc573082dc0215efe175ebb334
Diogo Vieira <diogovieiraengineer@gmail.com>
2024-06-07 15:50:20 +0100

    redirecionamento para a dashboard donors

--- angular-client/src/app/pages/donor-dashboard/donor-dashboard.component.ts
+++ angular-client/src/app/pages/donor-dashboard/donor-dashboard.component.ts
@@ -1,36 +1,177 @@
 import { Component, OnInit } from '@angular/core';
-import {  DashboardServiceService } from '../services/dashboard-service.service';
-import { PointsChartComponent } from "../points-chart/points-chart.component";
+import { Chart, registerables } from 'chart.js';
+import { HttpClient } from '@angular/common/http';
+import { NgModel } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { RestService } from '../rest.service';
+Chart.register(...registerables);
+NgModel;
 
 @Component({
   selector: 'app-donor-dashboard',
-  imports: [PointsChartComponent,],
+  imports: [],
   standalone: true,
   templateUrl: './donor-dashboard.component.html',
   styleUrls: ['./donor-dashboard.component.css']
 })
 export class DonorDashboardComponent implements OnInit {
-  totalDonations!: number;
-  totalPoints!: number;
-  pointsOverTime!: any[];
+  data: any;
+  entityData: any;
+  entityID: string | null = ' ';
 
-  constructor(private dashboardService: DashboardServiceService) {}
+  barChartLabelData: string[] = [];
+  barChartValueData: number[] = [];
+
+  pieChartLabelData: string[] = [];
+  pieChartValueData: number[] = [];
+
+  doughnutChartLabelData: string[] = [];
+  doughnutChartValueData: number[] = [];
+  donorID: string | null = ' ';
+  donorData!: any;
+
+  constructor(private authService: AuthService,
+    private rest: RestService,
+    private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.loadKPIs();
-    this.loadPointsOverTime();
+    this.donorID = this.authService.getUserIdFromToken();
+    console.log('Donor ID:', this.donorID);
+    this.getDonations();
   }
 
-  loadKPIs(): void {
-    this.dashboardService.getEntityKPIs().subscribe(data => {
-      this.totalDonations = data.totalDonations;
-      this.totalPoints = data.totalPoints;
+  logout(): void {
+    alert('Sessão terminada');
+    this.authService.loggout();
+  }
+
+  loadDonorData(donorID: string): void {
+    this.http
+      .get(`http://localhost:3000/donors/list/${donorID}/api`)
+      .subscribe((entity) => {
+        this.donorData = entity;
+      });
+  }
+
+  getDonations() {
+    this.rest.getDonations().subscribe((data) => {
+      console.log(data);
+      this.data = data;
+
+      if (this.data != null) {
+        this.data.forEach((data: any) => {
+          let barIndex = this.barChartLabelData.indexOf(data.donor.name);
+          if (data.entity.id === this.entityID) {
+            if (barIndex === -1) {
+              this.barChartLabelData.push(data.donor.name);
+              this.barChartValueData.push(data.points);
+            } else {
+              this.barChartValueData[barIndex] += data.points;
+            }
+          }
+          this.pieChartLabelData.push(data.condition);
+          this.pieChartValueData.push(data.kg);
+
+          let doughnutIndex = this.doughnutChartLabelData.indexOf(
+            data.donor.name
+          );
+
+          if (data.entity.id === this.entityID) {
+            if (doughnutIndex === -1) {
+              this.doughnutChartLabelData.push(data.donor.name);
+              this.doughnutChartValueData.push(data.kg);
+            } else {
+              this.doughnutChartValueData[doughnutIndex] += data.kg;
+            }
+          }
+        });
+      }
+
+      this.renderBarChart(
+        this.barChartLabelData,
+        this.barChartValueData,
+        'barChart',
+        'Points earned'
+      );
+      this.renderPieChart(
+        this.pieChartLabelData,
+        this.pieChartValueData,
+        'pieChart',
+        'Kgs'
+      );
+      this.renderDoughnutChart(
+        this.doughnutChartLabelData,
+        this.doughnutChartValueData,
+        'pieChart2',
+        'Kgs'
+      );
     });
   }
 
-  loadPointsOverTime(): void {
-    this.dashboardService.getPointsOverTime().subscribe(data => {
-      this.pointsOverTime = data;
+  renderBarChart(labelData: any, valueData: any, chartID: any, label: any) {
+    const chart = new Chart(chartID, {
+      type: 'bar',
+      data: {
+        labels: labelData,
+        datasets: [
+          {
+            label: label,
+            data: valueData,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
     });
   }
+
+  renderPieChart(labelData: any, valueData: any, chartID: any, label: any) {
+    const chart = new Chart(chartID, {
+      type: 'pie',
+      data: {
+        labels: labelData,
+        datasets: [
+          {
+            label: label,
+            data: valueData,
+            hoverOffset: 5,
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+      },
+    });
+  }
+
+  renderDoughnutChart(
+    labelData: any,
+    valueData: any,
+    chartID: any,
+    label: any
+  ) {
+    const chart = new Chart(chartID, {
+      type: 'doughnut',
+      data: {
+        labels: labelData,
+        datasets: [
+          {
+            label: label,
+            data: valueData,
+            hoverOffset: 5,
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+      },
+    });
+  }
+  
 }
